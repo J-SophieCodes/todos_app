a62b1c32d4a95ae734c69bb799ccfab5bb3d68b2
Sophie C <sophie@Sophies-MacBook-Pro.local>
2020-10-16 14:36:38 -0400

    Version 2 commit

--- todo.rb
+++ todo.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader' #if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
@@ -10,7 +10,7 @@ configure do
 end
 
 before do
-  session[:lists] ||= []
+  @storage = SessionPersistence.new(session)
 end
 
 helpers do
@@ -39,13 +39,119 @@ helpers do
   end
 end
 
+class SessionPersistence
+  def initialize(session)
+    @session = session
+    @session[:lists] ||= []
+  end
+
+  def find_list(index)
+    find_item(index, all_lists)
+  end
+
+  def all_lists
+    @session[:lists]
+  end
+
+  def existing_list?(list_name)
+    all_lists.any? { |list| list[:name].downcase == list_name.downcase }
+  end
+
+  def new_list(list_name)
+    index = next_idx(all_lists)
+    all_lists << { idx: index, name: list_name, todos: [] }
+  end
+
+  def delete_list(index)
+    delete_item(index, all_lists)
+  end
+
+  def update_list_name(index, new_name)
+    list = find_list(index)
+    list[:name] = new_name
+  end
+
+  def create_new_todo(list_idx, todo)
+    list = find_list(list_idx)
+    item_idx = next_idx(list[:todos])
+    list[:todos] << { idx: item_idx, name: todo, completed: false }
+  end
+
+  def existing_todo?(list_idx, todo)
+    list = find_list(list_idx)
+    list[:todos].any? { |item| item[:name].downcase == todo.downcase }
+  end
+
+  def delete_todo(list_idx, item_idx)
+    list = find_list(list_idx)
+    delete_item(item_idx, list[:todos])
+  end
+
+  def update_todo_status(list_idx, item_idx, new_status)
+    list = find_list(list_idx)
+    todo = find_item(item_idx, list[:todos])
+    todo[:completed] = new_status
+  end
+
+  def mark_all_todos_as_completed(list_idx)
+    list = find_list(list_idx)
+    list[:todos].each { |todo| todo[:completed] = true }
+  end
+
+  private
+
+  # Returns the next index in a list of todos or in a list of lists
+  def next_idx(list)
+    return 0 if list.empty?
+    list.map { |item| item[:idx] }.max + 1
+  end
+
+  # Delete item from a list of todos or from a list of lists
+  def delete_item(idx, list)
+    list.reject! { |item| item[:idx] == idx }
+  end
+
+  # Returns item from a list of todos or from a list of lists
+  def find_item(idx, list)
+    list.find { |item| item[:idx] == idx }
+  end
+end
+
+# Returns specified list from all lists
+def load_list(index)
+  list = @storage.find_list(index)
+  return list if list
+
+  session[:error] = 'The specified list was not found.'
+  redirect '/lists'
+end
+
+# Return an error message if the name is invalid. Otherwise, return nil.
+def detect_list_name_error(list_name)
+  if !(1..100).cover? list_name.size
+    'List name must be between 1 and 100 characters.'
+  elsif @storage.existing_list?(list_name)
+    "There is an existing '#{list_name}' list. Please provide a unique list name."
+  end
+end
+
+# Return an error message if the name is invalid. Otherwise, return nil.
+def detect_item_name_error(list_idx, todo)
+  if todo.nil? || todo.length.zero?
+    'Item description must be at least 1 character in length.'
+  elsif @storage.existing_todo?(list_idx, todo)
+    "'#{todo}' is already on the list."
+  end
+end
+
+# Redirect visit to homepage
 get '/' do
   redirect '/lists'
 end
 
 # View list of lists
 get '/lists' do
-  @lists = session[:lists]
+  @lists = @storage.all_lists
 
   erb :lists
 end
@@ -53,15 +159,6 @@ end
 # Render the new list form
 get '/lists/new' do
   erb :new_list
-end
-
-# Return an error message if the name is invalid. Otherwise, return nil.
-def detect_list_name_error(list_name)
-  if !(1..100).cover? list_name.size
-    'List name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name].downcase == list_name.downcase }
-    "There is an existing '#{list_name}' list. Please provide a unique list name."
-  end
 end
 
 # Create a new list
@@ -73,21 +170,13 @@ post '/lists' do
     session[:error] = error
     erb :new_list
   else
-    index = next_idx(session[:lists])
-    session[:lists] << { idx: index, name: list_name, todos: [] }
+    @storage.new_list(list_name)
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
 end
 
-def load_list(index)
-  list = find_item(index, session[:lists])
-  return list if list
-
-  session[:error] = 'The specified list was not found.'
-  redirect '/lists'
-end
-
+# View a todo list
 get '/lists/:index' do
   @index = params[:index].to_i
   @todo_list = load_list(@index)
@@ -95,7 +184,7 @@ get '/lists/:index' do
   erb :todo_list
 end
 
-#  Edit an existing todo list
+# Edit an existing todo list
 get '/lists/:index/edit' do
   @index = params[:index].to_i
   @todo_list = load_list(@index)
@@ -114,17 +203,16 @@ post '/lists/:index' do
     session[:error] = error
     erb :edit_list
   else
-    @todo_list[:name] = @list_name
+    @storage.update_list_name(@index, @list_name)
     session[:success] = 'The list has been updated.'
     redirect "/lists/#{@index}"
   end
 end
 
-#  Delete an existing todo list
+# Delete an existing todo list
 post '/lists/:index/destroy' do
   index = params[:index].to_i
-  list = find_item(index, session[:lists])
-  session[:lists].delete(list)
+  @storage.delete_list(index)
   session[:success] = 'The list has been deleted.'
   
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
@@ -134,50 +222,28 @@ post '/lists/:index/destroy' do
   end
 end
 
-# Return an error message if the name is invalid. Otherwise, return nil.
-def detect_item_name_error(todo, todolist)
-  if todo.nil? || todo.length.zero?
-    'Item description must be at least 1 character in length.'
-  elsif todolist.any? { |item| item[:name].downcase == todo.downcase }
-    "'#{todo}' is already on the list."
-  end
-end
-
-def next_idx(list)
-  return 0 if list.empty?
-  list.map { |item| item[:idx] }.max + 1
-end
-
 # Add a todo item
 post '/lists/:index/todos' do
   @index = params[:index].to_i
   @todo_list = load_list(@index)
   @todo = params[:todo].strip.squeeze(" ")
 
-  error = detect_item_name_error(@todo, @todo_list[:todos])
+  error = detect_item_name_error(@index, @todo)
   if error
     session[:error] = error
     erb :todo_list
   else
-    idx = next_idx(@todo_list[:todos])
-    @todo_list[:todos] << { idx: idx, name: @todo, completed: false }
+    @storage.create_new_todo(@index, @todo)
     session[:success] = 'The todo item has been added.'
     redirect "/lists/#{@index}"
   end
 end
 
-def find_item(idx, list)
-  list.find { |item| item[:idx] == idx }
-end
-
 # Delete a todo item
 post '/lists/:index/todos/:item_idx/destroy' do
   @index = params[:index].to_i
-  @todo_list = load_list(@index)
-
   item_idx = params[:item_idx].to_i
-  item = find_item(item_idx, @todo_list[:todos])
-  @todo_list[:todos].delete(item)
+  @storage.delete_todo(@index, item_idx)
 
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
@@ -187,29 +253,22 @@ post '/lists/:index/todos/:item_idx/destroy' do
   end
 end
 
-# Check off a completed item  
+# Update status of a todo item
 post '/lists/:index/todos/:item_idx' do
   @index = params[:index].to_i
-  @todo_list = load_list(@index)
-
   item_idx = params[:item_idx].to_i
-  item = find_item(item_idx, @todo_list[:todos])
-
   is_completed = params[:completed] == "true"
-  item[:completed] = is_completed
 
-  session[:success] = "'#{item[:name]}' is completed." if item[:completed]
+  @storage.update_todo_status(@index, item_idx, is_completed)
+
+  session[:success] = "The todo has been updated."
   redirect "/lists/#{@index}"
 end
 
 # To complete all todos
 post '/lists/:index/complete_all' do
   @index = params[:index].to_i
-  @todo_list = load_list(@index)
-
-  @todo_list[:todos].each do |todo|
-    todo[:completed] = true
-  end
+  @storage.mark_all_todos_as_completed(@index)
 
   session[:success] = 'All done!'
   redirect "/lists/#{@index}"
